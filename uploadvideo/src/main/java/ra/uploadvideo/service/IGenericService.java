b016cce6d07a1a3b1954de24c2f44ca7a3e09233
phamtuanlinh25121997 <128030831+phamtuanlinh25121997@users.noreply.github.com>
2023-08-18 10:42:49 +0700

    nb

---
+++ uploadvideo/src/main/java/ra/uploadvideo/service/IGenericService.java
@@ -0,0 +1,10 @@
+package ra.uploadvideo.service;
+
+import java.util.List;
+
+public interface IGenericService<T,E> {
+    List<T> findAll();
+    T findById(E e);
+    void save (T t);
+    void delete(E e);
+}
